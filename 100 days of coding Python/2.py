55628b69eae5d9aa43b819e01c34b508e1a36ace
1nnsane <dj.diko1337@gmail.com>
2024-03-31 14:53:05 -0500

    day 2

--- 100 days of coding Python/2.py
+++ 100 days of coding Python/2.py
@@ -26,3 +26,68 @@ new_num_char = str(num_char)
 print("==================================================")
 print("Type of new_num_char (len of name):", type(new_num_char))
 print("Method 2 - Your name has an: " + new_num_char + " characters") #giver error, because
+
+#= 21 lesson project ==========================================
+#input 39 -> output 12(3+9) || input 43 -> output 7(4+3)
+two_digit_numbers = input() #get string -> 39,42
+#Method 1
+print(type(two_digit_numbers)) #39 or 42 = str
+print(two_digit_numbers[0] + two_digit_numbers[1])
+print("Method 1: ", int(two_digit_numbers[0]) + (int(two_digit_numbers[1])))
+
+#Method 2 (more readable)
+first_digit = int(two_digit_numbers[0])
+second_digit = int(two_digit_numbers[1])
+print("Method 2: ", first_digit + second_digit)
+
+#= 22 Mathematical Operations =================================
+#priority -> first "()", second "**", thirth "*" or "/", forth "+" or "-"
+print(3 * 3 + 3 / 3 - 3) #7.0
+print(3 * (3 + 3) / 3 - 3) #3.0
+
+#= 23 BMI Calculator ==========================================
+height = float(input("ur height: ")) #1.75(metres)
+weight = int(input("ur weight: ")) #80(kg)
+bmi = (weight/height**2)
+print(bmi) #print float number
+print(int(bmi)) #print int number
+
+#= 24 Number Manipulation and F Strings =======================
+print(8/3) #gives float number 2.6666666666666665
+print(8//3) #chop numbers after "." = 2
+print(round(8/3)) #round to bigger or smaller number = 3
+print(round(8/3,3)) #round number, and gives 3 numbers and "." = 2.667
+
+name = "Diko"
+isWinning = True
+height = 1.8
+score = 0
+score += 2 # or (-=, *=, /=)
+print(score)
+#to print 2 and more data types, its neccesary to give them one data type - which is not comfortable
+#then we using f-string
+print(f"Ur score is {score}, ur name is {name}, u are winning is {isWinning}, and ur height is {height}")
+
+#= 25 Life in Weeks calculator =======================
+age = int(input("Ur age?: "))
+weeks_in_year = 365//7
+total_weeks = 90 * weeks_in_year
+left_weeks = (total_weeks - (age * weeks_in_year))
+
+print(f"U got a totally: {total_weeks} weeks")
+print(f"U have left: {left_weeks} weeks")
+
+#= 26 Project Tip Calculator =======================
+print("You using a tip Calculator")
+total_bill = float(input("What was your total bill?: "))
+tips_total = float(input("What the procent of tips u want left?: "))
+amount_people = float(input("for many people eat?: "))
+tips_per_each = ((total_bill/100) * tips_total)
+
+#Method 1, answer =  33.6 b
+total_for_each = round(((total_bill+tips_per_each)/amount_people), 2)
+print("Method 1: Each have to pay:", total_for_each)
+
+#Method 2, answer = 33.60 by using - "{:.2f}".format()
+total_for_each = "{:.2f}".format(((total_bill+tips_per_each)/amount_people))
+print("Method 2: Each have to pay:", total_for_each)
