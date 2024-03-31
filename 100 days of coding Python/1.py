f9e2a7cb4070b71668ca42c0453bd128834e516c
1nnsane <dj.diko1337@gmail.com>
2024-03-31 01:01:26 -0500

    input operations

--- 100 days of coding Python/1.py
+++ 100 days of coding Python/1.py
@@ -1,41 +1,48 @@
 # https://www.youtube.com/watch?v=KckAaeVob74 (pushed by this video)
-#
-# print("hello world")
-# print("hello \"world\"")
-# print("hello" + " world")
-#
-# print("hello " + input("what is your name?:\n"))
-# integer = input("choose number:\n")
-# print("ur number is - " + integer)
-#
-# print("i will print whatever u want: ")
-# print(input()) #prints whatever u type
-#
-# print("print 2 integer numbers: ")
-# num1 = int(input())
-# num2 = int(input())
-# print("sum of ur numbers is", int(num1+num2))
-#
-# NumberOfLetters = input(str)
+
+#DAY 1 COURSE
+#= 11 lesson ==================================================
+#print function rules
+print("hello world")
+print("hello \"world\"")
+print("hello" + " world\n")
+
+#simple program with inputs
+print("hello " + input("what is your name?:\n"))
+integer = input("choose number:\n")
+print("ur number is - " + integer)
+print("Ok, u typed:", input("i will print whatever u want: \n"))
+
+#sum of 2 numbers
+num1 = int(input("print 2 integer numbers:\n1 number: "))
+num2 = int(input("2 number: "))
+print("sum of ur numbers is", int(num1+num2))
+
+#define len of str you put
+print("Ur len is: ", len(input("I will calculate ur len of string word: \n"))) #Method 1
+# NumberOfLetters = input(str) #Method 2 easier to read
 # NumberOfLetters = len(NumberOfLetters)
 # print("NumberOfLetters - : ", str(NumberOfLetters))
-# print(len(input())) #define len of str u put
-#====================================================
-print("print 2 strings or integers") #lesson 14
-a = input() #41
-b = input() #29
-print("Method 1: a = ", b, "b = ", a) #swap 2 variables (1 method)
 
+#= 14 lesson ==================================================
+#swapping, method 1 (changing variables output)
+a = input("print 2 strings or integers:\nFirst: ") #41
+b = input("Second: ") #29
+print("Method 1: a = ", b, "b = ", a)
+
+#swapping, method 2 (with thirth variable)
 c = a #c = 41
 a = b #a = 29
-b = c #b = 41 (2 method)
+b = c #b = 41
 print("Method 2: a = ", a, "b = ", b)
-#====================================================
-#lesson 16
+
+#= 16 lesson ==================================================
+#project band name generator
 print("Hello my Friend!")
 grow_city = input("What is the city u grow up in?:\n")
 pet_name = input("What is the name of ur pet?:\n")
 print("Ur city and pet band name is: " + grow_city + " " + pet_name)
+#==============================================================
 
-
+#DAY 2 COURSE
 
