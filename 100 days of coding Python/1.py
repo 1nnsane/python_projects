2c032b2e9a6a5c1487f2b18785e269abac8a75a1
1nnsane <dj.diko1337@gmail.com>
2024-03-31 00:39:23 -0500

    input operations

--- 100 days of coding Python/1.py
+++ 100 days of coding Python/1.py
@@ -1,21 +1,41 @@
 # https://www.youtube.com/watch?v=KckAaeVob74 (pushed by this video)
-print("hello world")
-print("hello \"world\"")
-print("hello" + " world")
+#
+# print("hello world")
+# print("hello \"world\"")
+# print("hello" + " world")
+#
+# print("hello " + input("what is your name?:\n"))
+# integer = input("choose number:\n")
+# print("ur number is - " + integer)
+#
+# print("i will print whatever u want: ")
+# print(input()) #prints whatever u type
+#
+# print("print 2 integer numbers: ")
+# num1 = int(input())
+# num2 = int(input())
+# print("sum of ur numbers is", int(num1+num2))
+#
+# NumberOfLetters = input(str)
+# NumberOfLetters = len(NumberOfLetters)
+# print("NumberOfLetters - : ", str(NumberOfLetters))
+# print(len(input())) #define len of str u put
+#====================================================
+print("print 2 strings or integers") #lesson 14
+a = input() #41
+b = input() #29
+print("Method 1: a = ", b, "b = ", a) #swap 2 variables (1 method)
 
-print("hello " + input("what is your name?:\n"))
-integer = input("choose number:\n")
-print("ur number is - " + integer)
+c = a #c = 41
+a = b #a = 29
+b = c #b = 41 (2 method)
+print("Method 2: a = ", a, "b = ", b)
+#====================================================
+#lesson 16
+print("Hello my Friend!")
+grow_city = input("What is the city u grow up in?:\n")
+pet_name = input("What is the name of ur pet?:\n")
+print("Ur city and pet band name is: " + grow_city + " " + pet_name)
 
-print("i will print whatever u want: ")
-print(input()) #prints whatever u type
 
-print("print 2 integer numbers: ")
-num1 = int(input())
-num2 = int(input())
-print("sum of ur numbers is", int(num1+num2))
-
-NumberOfLetters = input(str)
-NumberOfLetters = len(NumberOfLetters)
-print("NumberOfLetters - : ", str(NumberOfLetters))
 
