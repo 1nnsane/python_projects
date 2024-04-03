5c1d68d1255841f292598dc14e5df73d07a1ead8
1nnsane <dj.diko1337@gmail.com>
2024-04-03 00:52:40 -0500

    day 4

--- 100 days of coding Python/codenotes.py
+++ 100 days of coding Python/codenotes.py
@@ -6,3 +6,5 @@ random.randint(1, 3) #first - import random. random int numbers from 1 to 3
 random.random() * 5 #random float number till from 0 to 5 (because divide to 5), other wise 0-1.0
 
 fruits = ["banana", "cherry", "pineapple"]; print(fruits)
+vegetables = ["tomato", "potato", "spinach"]
+mix = [fruits + vegetables]; print(mix)
