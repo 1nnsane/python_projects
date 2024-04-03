62c84fb0b7b956b183c92ea8edc7567aa3d9be1b
1nnsane <dj.diko1337@gmail.com>
2024-04-03 00:48:27 -0500

    day 4

--- 100 days of coding Python/4.py
+++ 100 days of coding Python/4.py
@@ -15,3 +15,63 @@ if rand_side == 0:
 else:
     print(f"It's heads {rand_side}")
 
+#44 lists
+fruits = ["cherry", "apple", "pear"]
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey",
+                     "Georgia", "Connecticut", "Massachusetts"]
+print(states_of_america) #shows whole list
+print(states_of_america[1]) #shows [1]
+states_of_america[1] = "Pencilvania" #change [1] to Pencilvania
+print(states_of_america) #shows changed list
+states_of_america.extend(["Lalalend", "Angelaland"])
+print(states_of_america) #shows changed list with new added value
+
+#45 who pay bill
+names = ['Angela', 'Ben', 'Jenny', 'Michael', 'Chloe', 'Dan']
+whopay = random.randint(0, len(names)-1) #-1 because it might be overflow (len 6, but index 0-5)
+print(f"{names[whopay]} is going to buy the meal today! number [{whopay}] in list")
+
+#46 add one list to another
+fruit_list = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetable_list = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+dozen_dirty = [fruit_list + fruit_list]
+print(dozen_dirty)
+
+#quiz
+#what will show in print(fruitss)?
+fruitss = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+fruitss[-1] = "Melons" #-1 its last element in list -> replace by "Melons"
+fruitss.append("Lemons") #and in the end of list "Lemons"
+print(fruitss)
+#what will be printed?
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen[1][1]) #1 its second list(vegetables). 1 its second element in second list ("Kale")
+
+#47 Treasure MAP
+line1 = [" ", "️ ", "️️ "]
+line2 = [" ", " ", "️ "]
+line3 = [" ", " ", "️ "]
+map = [line1, line2, line3]
+print("Hiding your treasure! X marks the spot.")
+position = input() # Where do you want to put the treasure?
+
+letter = position[0].lower()
+abc = ["a", "b", "c"]
+letter_index = abc.index(letter)
+number_index = int(position[1])-1
+map[number_index][letter_index] = "X"
+print(f"{line1}\n{line2}\n{line3}")
+
+#===my version is totally failed:=========
+# number_of_list = str(position[0]) #b3
+# if (position > 'A1' or 'B1' or 'C1'):
+#   line1[number_of_list-1] = 'X'
+# elif (position > 'A2' or 'B2' or 'C2'):
+#   line2[number_of_list-1] = 'X'
+# elif (position > 'A3' or 'B3' or 'C3'):
+#   line3[number_of_list-1] = 'X'
+# =========================================
+
+#48 Project Rock, Paper, Scissors (didn't finished yet)
---
+++ 100 days of coding Python/codenotes.py
@@ -0,0 +1,8 @@
+import random
+#https://docs.python.org/3/tutorial/datastructures.html data structures documentation
+
+x = len('helloworld'); print(x)
+random.randint(1, 3) #first - import random. random int numbers from 1 to 3
+random.random() * 5 #random float number till from 0 to 5 (because divide to 5), other wise 0-1.0
+
+fruits = ["banana", "cherry", "pineapple"]; print(fruits)
