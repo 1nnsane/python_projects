620b1773691adf2295207d9d97950c2df86222f8
1nnsane <dj.diko1337@gmail.com>
2024-04-06 00:04:42 -0500

    day 5

--- 100 days of coding Python/5.py
+++ 100 days of coding Python/5.py
@@ -57,3 +57,14 @@ for number in range(1, target + 1):
   if number % 2 == 0:
     alternative_sum += number
 print(alternative_sum)
+
+#56
+for i in range (1, 101):
+    if i % 15 == 0: #if start from 3 or 5 code will not print FizzBuzz
+        print("FizzBuzz")
+    elif i % 5 == 0:
+        print("Buzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    else:
+        print(i)
