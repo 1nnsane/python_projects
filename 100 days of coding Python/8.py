abee8e8e390ae75abf8783033f1ed3098c8d3c11
1nnsane <dj.diko1337@gmail.com>
2024-04-10 11:07:23 -0500

    day 7

---
+++ 100 days of coding Python/8.py
@@ -0,0 +1,13 @@
+#82 functions with inputs
+def greet():
+    print("hello greet")
+    print("hello greet")
+    print("hello greet")
+greet()
+
+def greet_with_name(name):
+    print(f"hello {name}")
+    print(f"how are you {name}")
+    print(f"isn't it a green weather, {name}")
+greet_with_name("Angela")
+
