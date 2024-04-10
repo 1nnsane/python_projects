5fc051213d6f0a7bfd59ec16bfb27487975c7e6f
1nnsane <dj.diko1337@gmail.com>
2024-04-10 12:40:02 -0500

    day 8

--- 100 days of coding Python/8.py
+++ 100 days of coding Python/8.py
@@ -10,3 +10,20 @@ def greet_with_name(name):
     print(f"how are you {name}")
     print(f"isn't it a green weather, {name}")
 greet_with_name("Angela")
+
+
+#85 prime checker
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+    if is_prime:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
+
+n = int(input("print number, i will check if its prime or not: "))  # Check this number
+prime_checker(number=n)
+
+#86
