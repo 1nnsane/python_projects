0a4628b75fd98edede80961f0ef4f83c26d7b400
1nnsane <dj.diko1337@gmail.com>
2024-04-14 23:58:24 -0500

    GH(HW)

---
+++ 100 days of coding Python/lesson4_GH.py
@@ -0,0 +1,36 @@
+# 4.3.1 Sum an array
+array1 = [1, 2, 3, 4, 5]
+array2 = [6, 7, 8, 9, 10]
+array1_and_2 = array1 + array2  # just adding list of array1 and array2
+print(array1_and_2)
+
+sum_of_array = 0
+for i in range(0, len(array1)):
+    # adding items of: array1[0] + array2[0] = 7, array1[1] + array2[2] = 9 ...
+    sum_of_array = array1[i] + array2[i]
+    print(sum_of_array)
+print("==========================================================")
+
+# 4.3.2 Reverse an array
+array1 = [1, 3, 5, 4, 2]
+print(array1)
+
+reverse_array = array1[::-1]
+print(reverse_array)
+
+# 4.3.3 Check if array is palindromic
+polin = False
+if array1 == array1[::-1]:
+    polin = True
+else:
+    polin = False
+print(f"array: {array1} is palindromic: {polin}")
+
+# 4.3.4 Remove duplicates from a sorted array
+array_sorted = [1, 2, 3, 4, 5, 5]
+checked = 0
+for i in range(0, len(array_sorted)):
+    if array_sorted[i] == len(array_sorted):
+        break
+    elif array_sorted[i] == array_sorted[i+1]:
+        print("Find")
