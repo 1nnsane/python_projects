907bd6bfdbc42135be55d64a0f68114fa57b50e3
1nnsane <dj.diko1337@gmail.com>
2024-04-14 22:55:16 -0500

    mut/immutable

---
+++ 100 days of coding Python/mutable_immutable.py
@@ -0,0 +1,21 @@
+#  IMMUTABLE
+a = "corey"
+print(a)
+print(format(id(a)))  # address of a
+
+a = "john"
+print(a)
+print(format(id(a)))  # address of new copy a
+# a[0] = "C" #can't change (doesn't support item assignment) because this is IMMUTABLE
+
+print("==============================================================")
+
+#  MUTABLE - LIST
+# modifying list, address is same
+a = [1, 2, 3, 4, 5]
+print(a)
+print(format(id(a)))
+
+a[0] = 6  # modified, address of a doesn't changed
+print(a)
+print(format(id(a)))
