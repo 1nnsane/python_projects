dffbbbdfddae87660548af434e489f6baea9908e
1nnsane <dj.diko1337@gmail.com>
2024-04-22 21:16:42 -0500

    day 10

---
+++ 100 days of coding Python/10.py
@@ -0,0 +1,11 @@
+#100 Functions with output
+def format_name(f_name, l_name):
+    f_name = f_name.title()
+    l_name = l_name.title()
+    return (f"{f_name} {l_name}")
+
+name = format_name("anGeLA", "lU")
+print(name)
+
+
+
